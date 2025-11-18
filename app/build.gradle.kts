aa61cad3781a01177f77e15e730c8abb88ba78aa
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2025-11-18 20:24:07 -0300

    andriod estudio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(platform("androidx.compose:compose-bom:2025.01.00"))
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.activity:activity-compose:1.9.2")
+
 }
